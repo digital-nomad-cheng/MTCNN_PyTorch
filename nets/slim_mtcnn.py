aa0b362b91dd9846d9422a5740cac9d45d6480a2
vincent <vincentcheng929@gmail.com>
2020-05-22 15:27:53 +0800

    Add slim MTCNN models

--- nets/slim_mtcnn.py
+++ nets/slim_mtcnn.py
@@ -1,3 +1,6 @@
+'''PNet, RNet, ONet, inspired from shufflenet and mobilenet'''
+
+import math
 from collections import OrderedDict
 
 import torch
@@ -27,6 +30,45 @@ def channel_shuffle(x, groups):
     
     return x
 
+class ShuffleConv2d(nn.Module):
+    def __init__(self, inp, oup, kernel_size=3, stride=2, groups=2):
+        super(ShuffleConv2d, self).__init__()
+        self.groups = groups
+        inp_per_group = inp // groups
+        oup_per_group = oup // groups
+        self.shuffle_conv = nn.Sequential(
+            nn.Conv2d(inp_per_group, inp_per_group, kernel_size=kernel_size,
+                stride=stride, groups=inp_per_group),
+            nn.PReLU(),
+            nn.Conv2d(inp_per_group, oup_per_group, kernel_size=1, stride=1),
+            nn.PReLU()
+        )
+
+    def forward(self, x):
+        x_list = list(x.chunk(self.groups, dim=1))
+        for i, x in enumerate(x_list):
+            x_list[i] = self.shuffle_conv(x)
+        x = torch.cat(x_list, dim=1)
+        x = channel_shuffle(x, self.groups)
+        return x
+
+class DepthwiseConv2d(nn.Module):
+    def __init__(self, inp, oup, stride):
+        super(DepthwiseConv2d, self).__init__()
+        self.use_res = (inp == oup) and (stride == 1)
+        self.feature = nn.Sequential(
+            nn.Conv2d(inp, inp, kernel_size=3, stride=stride, groups=inp),
+            nn.PReLU(),
+            nn.Conv2d(inp, oup, kernel_size=1, stride=1),
+            nn.PReLU()
+        )
+    
+    def forward(self, x):
+        out = self.feature(x)
+        if self.use_res:
+            out = out + x
+        return out
+
 class Flatten(nn.Module):
     def __init__(self):
         super(Flatten, self).__init__()
@@ -40,151 +82,150 @@ class PNet(nn.Module):
     '''12*12 stride 2'''
     def __init__(self, is_train=False):
         super(PNet, self).__init__()
+        
         self.is_train = is_train
-
-        '''
-        conv1: (H-2)*(W-2)*10
-        prelu1: (H-2)*(W-2)*10
         
-        pool1: ((H-2)/2)*((W-2)/2)*10
-        
-        conv2: ((H-2)/2-2)*((W-2)/2-2)*16
-        prelu2: ((H-2)/2-2)*((W-2)/2-2)*16
-        
-        conv3: ((H-2)/2-4)*((W-2)/2-4)*32
-        prelu3: ((H-2)/2-4)*((W-2)/2-4)*32
-        
-        conv4_1: ((H-2)/2-4)*((W-2)/2-4)*2
-        conv4_2: ((H-2)/2-4)*((W-2)/2-4)*4
+        self.conv1 = nn.Sequential(
+                nn.Conv2d(3, 12, kernel_size=3, stride=1),
+                nn.PReLU(),
+                nn.MaxPool2d(2, 2, ceil_mode=False)
+        )
+        self.conv2 = ShuffleConv2d(12, 24, 3, 1, 2)
+        self.conv3 = ShuffleConv2d(24, 32, 3, 1, 2)
 
-        The last feature map size is: (H - 10)/2 = (H - 12)/2 + 1.
-        Thus the effect of PNet equals to moving 12*12 convolution window with 
-        kernel size 12, stirde 2.
-        '''
-
-        self.features = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(3, 10, 3, 1)),
-            ('prelu1', nn.PReLU(10)),
-            ('pool1', nn.MaxPool2d(2, 2, ceil_mode=False)),
-            
-            ('conv2', nn.Conv2d(10, 16, 3, 1)),
-            ('prelu2', nn.PReLU(16)),
-
-            ('conv3', nn.Conv2d(16, 32, 3, 1)),
-            ('prelu3', nn.PReLU(32)),
-        ]))
-
-        self.conv4_1 = nn.Conv2d(32, 2, 1, 1)
-        self.conv4_2 = nn.Conv2d(32, 4, 1, 1)
+        self.conv4_1 = nn.Conv2d(32, 2, kernel_size=1)
+        self.conv4_2 = nn.Conv2d(32, 4, kernel_size=1)
 
     def forward(self, x):
-        x = self.features(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        
         scores = self.conv4_1(x)
         offsets = self.conv4_2(x)
         
         # append softmax for inference
         if not self.is_train:
-            socres = F.softmax(scores, dim=1)
-
+            scores = F.softmax(scores, dim=1)
+        
         return scores, offsets
 
 class RNet(nn.Module):
-    '''Input size should be 24*24*3'''
     def __init__(self, is_train=False):
         super(RNet, self).__init__()
         self.is_train = is_train
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, stride=1),
+            nn.PReLU(),
+            nn.MaxPool2d(3, 2, ceil_mode=False)
+        )
+        self.conv2 = nn.Sequential(
+            ShuffleConv2d(32, 64, 3, 1, 2),
+            nn.MaxPool2d(3, 2, ceil_mode=False),
+        )
+        self.conv3 = nn.Sequential(
+            ShuffleConv2d(64, 128, 2, 1, 2)
+        )
+        self.conv4 = nn.Sequential(
+            Flatten(),
+            nn.Linear(128*2*2, 128),
+            nn.PReLU()
+        )
         
-        self.features = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(3, 28, 3, 1)), # 24 -2 = 22
-            ('prelu1', nn.PReLU(28)),
-            ('pool1', nn.MaxPool2d(3, 2, ceil_mode=False)), # (22-3)/2 + 1 = 10
-
-            ('conv2', nn.Conv2d(28, 48, 3, 1)), # 10 - 2 = 8
-            ('prelu2', nn.PReLU(48)),
-            ('pool2', nn.MaxPool2d(3, 2, ceil_mode=False)), # (8-3)/2 + 1 = 3
-
-            ('conv3', nn.Conv2d(48, 64, 2, 1)), #  3 - 1 = 2
-            ('prelu3', nn.PReLU(64)),
-
-            ('flatten', Flatten()),
-            ('conv4', nn.Linear(64*2*2, 128)),
-            ('prelu4', nn.PReLU(128))
-        ]))
-
         self.conv5_1 = nn.Linear(128, 2)
         self.conv5_2 = nn.Linear(128, 4)
+        
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+    
     def forward(self, x):
-        x = self.features(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        
         scores = self.conv5_1(x)
         offsets = self.conv5_2(x)
-
+        
         if not self.is_train:
             scores = F.softmax(scores, dim=1)
+        
         return scores, offsets
 
 class ONet(nn.Module):
-    '''Input size should be 48*48*3'''
     def __init__(self, is_train=False, train_landmarks=False):
         super(ONet, self).__init__()
-        
         self.is_train = is_train
         self.train_landmarks = train_landmarks
-
-        self.features = nn.Sequential(OrderedDict([
-            ('conv1', nn.Conv2d(3, 32, 3, 1)), # 48 - 2 = 46
-            ('prelu1', nn.PReLU(32)),
-            ('pool1', nn.MaxPool2d(3, 2, ceil_mode=False)), # (46-3)/2 + 1 = 22 
-
-            ('conv2', nn.Conv2d(32, 64, 3, 1)), # 22 - 2 = 20
-            ('prelu2', nn.PReLU(64)),
-            ('pool2', nn.MaxPool2d(3, 2, ceil_mode=False)), # (20-3)/2 + 1 = 9
-
-            ('conv3', nn.Conv2d(64, 64, 3, 1)), # 9 - 2 = 7   
-            ('prelu3', nn.PReLU(64)),
-            ('pool3', nn.MaxPool2d(2, 2, ceil_mode=False)), # (7-2)/2 + 1 = 3
-            
-            ('conv4', nn.Conv2d(64, 128, 2, 1)), # 3 - 1 = 2
-            ('prelu4', nn.PReLU(128)),
-
-            ('flatten', Flatten()),
-            ('conv5', nn.Linear(128*2*2, 256)),
-            ('prelu5', nn.PReLU(256))
-        ]))
+        
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3),
+            nn.PReLU(32),
+            nn.MaxPool2d(3, 2, ceil_mode=False)
+        )
+        self.conv2 = nn.Sequential(
+            ShuffleConv2d(32, 64, 3, 1, 2),
+            nn.MaxPool2d(3, 2, ceil_mode=False)
+        )
+        self.conv3 = nn.Sequential(
+            ShuffleConv2d(64, 128, 3, 1, 2),
+            nn.MaxPool2d(2, 2, ceil_mode=False)
+        )
+        self.conv4 = nn.Sequential(
+            ShuffleConv2d(128, 256, 3, 1, 2)
+        )
+        self.conv5 = nn.Sequential(
+            Flatten(),
+            nn.Linear(256, 256),
+            nn.PReLU(256)
+        )
 
         self.conv6_1 = nn.Linear(256, 2)
         self.conv6_2 = nn.Linear(256, 4)
         self.conv6_3 = nn.Linear(256, 10)
-    
+
     def forward(self, x):
-        x = self.features(x)
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = self.conv5(x)
+        
         scores = self.conv6_1(x)
         offsets = self.conv6_2(x)
-        
+
         if not self.is_train:
             scores = F.softmax(scores, dim=1)
         
         if self.train_landmarks:
             landmarks = self.conv6_3(x)
+            
             return scores, offsets, landmarks
-       
+        
         return scores, offsets
 
-
 if __name__ == "__main__":
-    pnet = PNet(is_train=False)
-    pnet.load_state_dict(torch.load('./pretrained_weights/best_pnet.pth'))
-    torch.onnx.export(pnet, torch.randn(1, 3, 12, 12), './onnx2ncnn/pnet.onnx', 
-            input_names=['input'], output_names=['scores', 'offsets'])
-    #summary(pnet.cuda(), (3, 12, 12))
+    #device = torch.device("cuda:1")
+    #pnet = PNet(is_train=False)
+    #pnet.load_state_dict(torch.load('./pretrained_weights/best_pnet.pth'))
+    #torch.onnx.export(pnet, torch.randn(1, 3, 12, 12), './onnx2ncnn/pnet.onnx', 
+    #        input_names=['input'], output_names=['scores', 'offsets'])
+    #pnet.to(device)
+    #summary(pnet, (3, 12, 12))
     
-    rnet = RNet(is_train=False)
-    rnet.load_state_dict(torch.load('./pretrained_weights/best_rnet.pth'))
-    torch.onnx.export(rnet, torch.randn(1, 3, 24, 24), './onnx2ncnn/rnet.onnx', 
-            input_names=['input'], output_names=['scores', 'offsets'])
+     
+    #rnet = RNet(is_train=False)
+    #rnet.load_state_dict(torch.load('./pretrained_weights/best_rnet.pth'))
+    #torch.onnx.export(rnet, torch.randn(1, 3, 24, 24), './onnx2ncnn/rnet.onnx', 
+    #        input_names=['input'], output_names=['scores', 'offsets'])
     #summary(rnet.cuda(), (3, 24, 24)) 
     
-
     onet = ONet(is_train=False)
     #onet.load_state_dict(torch.load('./pretrained_weights/best_onet.pth'))
     #summary(onet.cuda(), (3, 48, 48))
+    torch.onnx.export(onet, torch.randn(1, 3, 48, 48), './onnx2ncnn/onet.onnx', 
+            input_names=['input'], output_names=['scores', 'offsets'])
